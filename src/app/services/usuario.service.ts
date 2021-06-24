28850057f676a34cb0d62c7a85d7cd650cb0d4e4
juan coy <juancoy@sqdm.com>
2021-06-24 13:18:56 -0500

    Adicion enviroment

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Usuario } from '../models/usuario.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +10,21 @@ export class UsuarioService {
 
   constructor(private http: HttpClient) { }
 
+  public url = environment.url;
 
   getUsuarios() {
-
-    return this.http.get('http://localhost:3000/api/usuarios');
+    return this.http.get(this.url);
   }
 
-  guardarUsuario(usuario: Usuario) {
+  guardarUsuario(usuario: Usuario): any {
 
-    return this.http.post('http://localhost:3000/api/usuarios', usuario);
+    return this.http.post(this.url, usuario);
   }
 
   actualizarUsuario(id: string, usuario: Usuario) {
     delete usuario._id;
 
-    return this.http.put(`http://localhost:3000/api/usuarios/${id}`, usuario);
+    return this.http.put(`${this.url}/${id}`, usuario);
   }
 
 }
